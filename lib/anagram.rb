15ecfaceb878c220846068452d7905e778a63595
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-09-13 20:06:51 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class AnagramDectector
+
+  attr_accessor :anagram
+
+anagram_words = []
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match (anagram_words)
+anagram_words.each do {|word| word.split("")}
+    if @anagram.include?(word.split("")) == true
+      true
+    else
+      false
+    end
+end
+  end

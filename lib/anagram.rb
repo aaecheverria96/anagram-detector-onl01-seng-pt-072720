31a7143e64740fd7efcf7bd77876f083cb201fd5
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-09-14 16:27:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,6 @@ def initialize(anagram)
 end
 
 def match (anagram_words)
-  no_matches = []
   anagram_words.select {|word| word.split("").sort == @anagram.split("").sort}
       #if @anagram.include?(word.split("")) == true
       #  true
